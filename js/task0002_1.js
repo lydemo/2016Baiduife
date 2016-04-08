81d44031e47cd7bad7d5d7e444e39120d00f9f98
ly199106 <xiaohaitun1991@126.com>
2016-04-08 11:10:17 +0800

    part1

---
+++ js/task0002_1.js
@@ -0,0 +1,36 @@
+﻿
+$.click("button", function() {
+
+   // var content = $("input").value;
+    var content = $("textarea").value;
+    console.log($("textarea"));
+    console.log(content);
+    content = trim(content); //去掉首位的空格
+    var contentArr = content.split(/\n|\s+|,|，|;|；|、/);//允许用户用换行、空格（全角/半角）、逗号（全角/半角）、顿号、分号来作为不同爱好的分隔
+    var contentArr2 = deleteBlank(uniqArray(contentArr)); 
+
+    var displayDiv = $(".hobby");
+   
+
+   if (contentArr2.length > 10 || contentArr2.length === 0) {
+        
+		if(contentArr2.length===0)
+        displayDiv.style.display = "none";
+   } else {
+        
+       var checkboxStr = "";
+        for (var i = 0; i < contentArr2.length; i++) {
+           //  contentArr2[i] = 0;
+            checkboxStr += '<br><input type="checkbox"><label>' + contentArr2[i] + '</label>';
+        }  
+        console.log(checkboxStr.substr(4));
+        displayDiv.innerHTML =contentArr2 ;
+        displayDiv.style.display = "block";
+    }
+
+    console.log(contentArr2);
+    $(".hobby").innerHTML = contentArr2;
+
+
+
+});
---
+++ js/util.js
@@ -0,0 +1,307 @@
+﻿/*判断arr是否为一个数组，返回一个bool值
+  在iframe里不能用instanceof判断类型
+*/
+
+function isArray(arr) {
+    // your implement
+	if(Object.prototype.toString.call(arr)=="[object Array]")
+	return true;
+	else
+	return false;
+}
+
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(fn) {
+    // your implement
+	if(typeof fn=== 'function')
+	return true;
+	else
+	return false;
+}
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
+    // your implement
+	var i;
+	if(typeof src==='number'||typeof src==='string'||typeof src==='boolean'||typeof src==='object'||checkDate(src))
+	{
+		
+	}
+	function checkDate(obj){
+    var strDate=obj.value;//获取对象的值
+    re=/^(\d{4})(\d{2})(\d{2})$/g;//正则表达式
+    if(re.test(strDate))//判断日期格式符合YYYY-MM-DD
+    {
+        return true;//是日期，返回true
+    }
+	else
+	    return false;
+}
+
+	
+}
+
+
+// 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
+function uniqArray(arr) {
+    // your implement
+	var newarr=[];
+	for(var i=0;i<arr.length;i++)
+	{
+	 if(newarr.indexOf(arr[i])==-1)
+		newarr.push(arr[i]);
+	}
+	return newarr;
+}
+/*
+ * 除去数组中的空元素
+ */
+function deleteBlank(arr) {
+    var arr2 = [];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].match(/\s+/) || arr[i] === "") {
+            continue;
+        } else {
+            arr2.push(arr[i]);
+        }
+    }
+    return arr2;
+}
+// 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
+function each(arr, fn) {
+    for (var i in arr) {
+        fn(arr[i], i);
+    }
+}
+// 中级班同学跳过此题
+// 实现一个简单的trim函数，用于去除一个字符串，头部和尾部的空白字符
+// 假定空白字符只有半角空格、Tab
+// 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
+function simpleTrim(str) {
+    var i;
+    var j;
+    for (i = 0; i < str.length; i++) { //从头遍历字符串
+        if (str.charAt(i) != " " && str.charAt(i) != "\t") { //当不为空的时候
+            break; //跳出循环
+        }
+    }
+    for (j = str.length - 1; j >= 0; j--) {
+        if (str.charAt(j) != " " && str.charAt(j) != "\t") { //当不为空的时候
+            break; //跳出循环
+        }
+    }
+    return str.slice(i, j + 1); //返回子字符串
+}
+// 很多同学肯定对于上面的代码看不下去，接下来，我们真正实现一个trim
+// 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
+// 尝试使用一行简洁的正则表达式完成该题目
+function trim(str) {
+    // your implement
+	if(str.length!=-1)
+	return str.replace(/^\s+|^\s+$|^\t+|^\t+$/g,'');
+}
+// 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
+function each(arr, fn) {
+    // your implement
+}
+
+// 其中fn函数可以接受两个参数：item和index
+// 获取一个对象里面第一层元素的数量，返回一个整数
+function getObjectLength(obj) {
+    return Object.keys(obj).length;
+}
+// 判断是否为邮箱地址
+function isEmail(emailStr) {
+    // your implement
+	var pattern = /^(\w+\.)*\w+@\w+(\.\w+)+$/;
+    
+	return pattern.test(emailStr);
+}
+
+// 判断是否为手机号
+function isMobilePhone(phone) {
+    // your implement
+	var pattern=/^(\+\d{1,4})?\d{7,11}$/;//手机号前面有"+"号,可能为11位，也可能是8位
+	return pattern.test(phone);
+}
+// 为element增加一个样式名为newClassName的新样式
+function addClass(element, newClassName) {
+    // your implement
+	 var oldClassName = element.className; //获取旧的样式类
+    element.className = oldClassName === "" ? newClassName : oldClassName + " " + newClassName;
+}
+// 移除element中的样式oldClassName
+function removeClass(element, oldClassName) {
+//    var originClassName = element.className; //获取原先的样式类
+//    var pattern = new RegExp("\\b" + oldClassName + "\\b"); //使用构造函数构造动态的正则表达式
+//    element.className = trim(originClassName.replace(pattern, ''));
+	  element.className='';
+}
+
+// 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
+function isSiblingNode(element, siblingNode) {
+    // your implement
+	function isSiblingNode(element, siblingNode) {
+    return element.parentNode === siblingNode.parentNode;
+}
+
+}
+
+// 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
+function getPosition(element) {
+    // your implement
+ var pos = {};
+    pos.x = element.getBoundingClientRect().left + Math.max(document.documentElement.scrollLeft, document.body.scrollLeft);
+    pos.y = element.getBoundingClientRect().top + Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    return pos;
+
+}
+// your implement
+// 实现一个简单的Query
+function $(selector) {
+
+    if (!selector) {
+        return null;
+    }
+
+    if (selector == document) {
+        return document;
+    }
+
+    selector = trim(selector);
+    if (selector.indexOf(" ") !== -1) { //若存在空格
+        var selectorArr = selector.split(/\s+/); //拆成数组
+
+        var rootScope = myQuery(selectorArr[0]); //第一次的查找范围
+        var i = null;
+        var j = null;
+        var result = [];
+        //循环选择器中的每一个元素
+        for (i = 1; i < selectorArr.length; i++) {
+            for (j = 0; j < rootScope.length; j++) {
+                result.push(myQuery(selectorArr[i], rootScope[j]));
+            }
+            // rootScope = result;
+            // 目前这个方法还有bug
+        }
+        return result[0][0];
+    } else { //只有一个，直接查询
+        return myQuery(selector, document)[0];
+    }
+}
+
+/**
+ * 针对一个内容查找结果 success
+ * @param  {String} selector 选择器内容
+ * @param  {Element} root    根节点元素
+ * @return {NodeList数组}    节点列表，可能是多个节点也可能是一个
+ */
+function myQuery(selector, root) {
+    var signal = selector[0]; //
+    var allChildren = null;
+    var content = selector.substr(1);
+    var currAttr = null;
+    var result = [];
+    root = root || document; //若没有给root，赋值document
+    switch (signal) {
+        case "#":
+            result.push(document.getElementById(content));
+            break;
+        case ".":
+            allChildren = root.getElementsByTagName("*");
+            // var pattern0 = new RegExp("\\b" + content + "\\b");
+            for (i = 0; i < allChildren.length; i++) {
+                currAttr = allChildren[i].getAttribute("class");
+                if (currAttr !== null) {
+                    var currAttrsArr = currAttr.split(/\s+/);
+                    // console.log(currAttr);
+                    for (j = 0; j < currAttrsArr.length; j++) {
+                        if (content === currAttrsArr[j]) {
+                            result.push(allChildren[i]);
+                            // console.log(result);
+                        }
+                    }
+                }
+            }
+            break;
+        case "[": //属性选择
+            if (content.search("=") == -1) { //只有属性，没有值
+                allChildren = root.getElementsByTagName("*");
+                for (i = 0; i < allChildren.length; i++) {
+                    if (allChildren[i].getAttribute(selector.slice(1, -1)) !== null) {
+                        result.push(allChildren[i]);
+                    }
+                }
+            } else { //既有属性，又有值
+                allChildren = root.getElementsByTagName("*");
+                var pattern = /\[(\w+)\s*\=\s*(\w+)\]/; //为了分离等号前后的内容
+                var cut = selector.match(pattern); //分离后的结果，为数组
+                var key = cut[1]; //键
+                var value = cut[2]; //值
+                for (i = 0; i < allChildren.length; i++) {
+                    if (allChildren[i].getAttribute(key) == value) {
+                        result.push(allChildren[i]);
+                    }
+                }
+            }
+            break;
+        default: //tag
+            result = root.getElementsByTagName(selector);
+            break;
+    }
+    return result;
+}
+
+// 给一个element绑定一个针对event事件的响应，响应函数为listener
+function addEvent(element, event, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(event, listener);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + event, listener);  //IE9以前不支持addEventListener,并且第一个参数使用带"on"前缀的事件处理程序属性名
+    }
+}
+// 移除element对象对于event事件发生时执行listener的响应
+function removeEvent(element, event, listener) {
+    if (element.removeEventListenr) {
+        element.removeEventListenr(event, listener);
+    } else if (element.detachEvent) {
+        element.detachEvent("on" + event, listener);//同上
+    }
+}
+// 实现对click事件的绑定
+function addClickEvent(element, listener) {
+    addEvent(element, "click", listener);
+}
+
+// 实现对于按Enter键时的事件绑定
+function addEnterEvent(element, listener) {
+    addEvent(element, "keydown", function(event) {
+        if (event.keyCode == 13) {
+            listener();
+        }
+    });
+}
+// 事件代理
+function delegateEvent(element,tag,eventName,listener){
+    addEvent(element, eventName, function(event){
+        var target = event.target || event.srcElement;
+        if(target.tagName.toLowerCase() == tag.toLowerCase()) {
+            listener.call(target, event);
+        }
+    });
+}
+// 估计有同学已经开始吐槽了，函数里面一堆$看着晕啊，那么接下来把我们的事件函数做如下封装改变：
+
+$.on = function(selector, event, listener) {
+    addEvent($(selector), event, listener);
+};
+$.click = function(selector, listener) {
+    addClickEvent($(selector), listener);
+};
+$.un = function(selector, event, listener) {
+    removeEvent($(selector), event, listener);
+};
+$.delegate = function(selector, tag, event, listener) {
+    delegateEvent($(selector), tag, event, listener);
+};
+
